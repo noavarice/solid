bef6530bf50d9807abb636ccb6f10568b9efdc9f
noavarice <noavarice@gmail.com>
2017-12-13 11:15:00 +0300

    added OCP 'Strategy' example

--- OCP/OCPStrategy/src/ocpstrategy/OCPStrategy.java
+++ OCP/OCPStrategy/src/ocpstrategy/OCPStrategy.java
@@ -5,17 +5,34 @@
  */
 package ocpstrategy;
 
+import java.util.Calendar;
+import java.util.Date;
+import src.CustomCalendar;
+import src.impl.GBDatePrinter;
+import src.impl.RussianDatePrinter;
+import src.impl.USADatePrinter;
+
 /**
  *
  * @author alexrazinkov
  */
 public class OCPStrategy {
+    
+    private static void print(final String s) {
+        System.out.println(s);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final Date currentDate = Calendar.getInstance().getTime();
+        CustomCalendar cal = new CustomCalendar(currentDate, new RussianDatePrinter());
+        print(cal.getFormattedDate());
+        cal.setDateFormatStrategy(new GBDatePrinter());
+        print(cal.getFormattedDate());
+        cal.setDateFormatStrategy(new USADatePrinter());
+        print(cal.getFormattedDate());
     }
     
 }
